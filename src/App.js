9ea5ae15111cec2faa027e1e8cef8571921ee2cf
Alex Wade <alex.wade13@gmail.com>
2016-12-20 11:10:13 -0500

    final commit?

--- src/App.js
+++ src/App.js
@@ -1,84 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Tower from './Tower'
 
-
 class App extends Component {
-
 
   constructor(props) {
     super(props)
     var tower = [];
-
+    //to have an intial screen of discs
     for (var i = 1; i < 5; i++) {
        tower.push(i);
     }
+    //solved determines if the current disc arrangment has a solution that has been computed
+    //solution will be an array of 2d arrays that hold the steps to take during the solution
+    //speed is in ms, which changes depending on which speed button is pressed
     this.state = {
       towers:[tower,[],[]],
       solved:false,
       solution:false,
-      speed: 1000
-
+      speed: 10,
+      playing: false
     }
   }
 
-  startHanoi(){
-    var solution = [];
-    var hanoiSolution = function(disc,src,aux,dst) {
-      if (disc > 0) {
-        hanoiSolution.apply(this,[disc - 1,src,dst,aux]);
-        // console.log("Move disc " + disc + " from " + src + " to " + dst + "<br />");
-        solution.push([src,dst])
-        hanoiSolution.apply(this,[disc - 1,aux,src,dst]);
-      }
-    };
-    
-    hanoiSolution.apply(this,[this.state.towers[0].length,0,1,2])
-    this.setState({solved:true,solution:solution},this.hanoiStep);
-    console.log(solution)
+  startHanoi(speed){
+    if(this.state.solved === false) {
+
+      var solution = [];
+      var hanoiSolution = function(disc,src,aux,dst) {
+        if (disc > 0) {
+          hanoiSolution.apply(this,[disc - 1,src,dst,aux]);
+          solution.push([src,dst])
+          hanoiSolution.apply(this,[disc - 1,aux,src,dst]);
+        }
+      };
+      
+      hanoiSolution.apply(this,[this.state.towers[0].length,0,1,2])
+      this.setState({playing:true,solved:true,solution:solution,speed:speed},this.hanoiStep);
+    }
   }
 
-  hanoiStep(){
-    if(this.state.solution.length !==0) {
 
+  //responsible for each individual step
+  hanoiStep(){
+
+    if(this.state.solution.length !==0 && this.state.playing) {
       var towers = this.state.towers;
       var solution = this.state.solution
       var step = solution.shift()
       towers[step[1]].unshift(towers[step[0]].shift())
-
-      
-      setTimeout(()=>{this.setState({solution:solution,towers:towers}, this.hanoiStep)}, this.state.speed)
-      
-      
+      //this statement controls the animation, allowing this function to run asynchonously
+      //setting the state while the next step begins its loop.
+      setTimeout(()=>{this.setState({solution:solution,towers:towers}, this.hanoiStep)}, this.state.speed)     
     }
-
   }
 
-  changeSize(){
+  reset(){
+
     var tower = []
     for (var i = 1; i <= document.getElementById("selector").value; i++) {
       tower.push(i);
     }
-    this.setState({towers:[tower,[],[]]})
-    console.log("test")
+
+    this.setState({solved:false,towers:[tower,[],[]]})
   }
 
   render() {
-    console.log(this.state)
+
     var options = []
+
     for(var i=1; i<55;i++){
       options.push(<option value={i} key={i}>{i}</option>);
     }
-    
 
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-          <button onClick={this.startHanoi.bind(this)}>solve</button>
-          <select id ="selector" onChange={this.changeSize.bind(this)} >
+          <h2>Hanoi Towers!</h2>
+          <button onClick={this.startHanoi.bind(this,.5)}>Solve Faster</button>
+          <button onClick={this.startHanoi.bind(this,200)}>Solve Slower</button>
+          <button onClick={this.reset.bind(this)}>Reset</button>
+          <select id ="selector" onChange={this.reset.bind(this)} >
             {options}
           </select>
         </div>
@@ -92,9 +94,7 @@ class App extends Component {
         <div className="col col3">
           <Tower tower={this.state.towers[2]} />
         </div>
-
       </div>
-
     );
   }
 }
--- src/Tower.js
+++ src/Tower.js
@@ -1,48 +1,41 @@
 import React, { Component } from 'react';
 
-
 export default class Tower extends React.Component {
-
 
   constructor(props) {
     super(props)
-    console.log(props)
-    this.state = {
-    	tower:props.tower
-
-    }
   }
 
-
 	render(){
-
-
 		var tower = this.props.tower
-    var divs =[]
-    var width;
-    var left;
-    var style;
-    var classes;
-    for(var i=this.props.tower.length -1;i >= 0 ; i--){
-      
-      width = tower[i] + "%";
-      left = 50 - tower[i]/2 + "%"
+    var divs = []
+    var width, left, style, classes, num, r, g, b, colorValue;
+    for(var i=this.props.tower.length -1;i >= 0 ; i--){      
+      //for determining the color of the disc
+      num = (tower[i] * 255 / 54);
+      r = Math.round(Math.sin(0.024 * num + 0) * 127 + 128);
+      g = Math.round(Math.sin(0.024 * num + 2) * 127 + 128);
+      b = Math.round(Math.sin(0.024 * num + 4) * 127 + 128);
+      colorValue = 'rgb(' + r + ',' + g + ',' + b + ')';
+
+      //determining the width of the disc based on which disc it is
+      width = tower[i]*1.8 + "%";
+      //determining how far in the disc will be
+      left = 50 - tower[i]*1.8/2 + "%"
       style = {
-        width:width,
-        left:left
+        "width":width,
+        "left":left,
+        "background-color":colorValue
       }
 
       classes = "disc disc" + tower[i]
-      divs.push(<div className={classes} style={style} key={i}></div>);
-      
-
+      //pushing all the divs onto an array to be returned
+      divs.push(<div className={classes} style={style} key={i}></div>);      
     }
-    return (<div>
 
+    return (<div>
     		{divs}
     		</div>
     	);
-
 	}
-
 }
