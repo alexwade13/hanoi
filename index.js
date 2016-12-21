5ebd75f9e69bf2ab1ebd0636f8ce12046c2a26bc
Alex Wade <alex.wade13@gmail.com>
2016-12-20 20:16:10 -0500

    server?

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const cors = require("cors");
+
+const port = process.env.PORT || 8888; 
+
+// Initialize the server 
+const app = express();
+// Add middleware 
+app.use(cors());
+app.use(bodyParser.json());
+
+// Set static file location
+app.use(express.static(__dirname + '/build'))
+
+app.listen(port);
