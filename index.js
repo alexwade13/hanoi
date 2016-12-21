55056d289a03ffe00dfc50332b23c2fe9d1ad357
Alex Wade <alex.wade13@gmail.com>
2016-12-20 22:18:38 -0500

    heroku trobules!

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.get('*', (req, res) => {
 const PORT = process.env.PORT || 9000;
  
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!!`);
 });
